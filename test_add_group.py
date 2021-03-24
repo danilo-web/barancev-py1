bcd652f119d9f8005b66ecbd1a0c7cf015f6b3be
daniil_sboev <danilo-web@mail.ru>
2021-03-23 22:17:44 -0400

    modified test for page object

--- test_add_group.py
+++ test_add_group.py
@@ -1,54 +1,80 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_homepage(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, group_name="test", group_header="test", group_footer="test")
+        self.retern_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, group_name="test", group_header="test", group_footer="test")
+        self.retern_to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def retern_to_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, group_name, group_header, group_footer):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_xpath("//div[@id='content']/h1").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("test")
+        wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("group_footer").send_keys(group_footer)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
 
-    
-    def tearDown(self):
-        self.wd.quit()
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_homepage(self, wd):
+        # open homepage
+        wd.get("http://localhost/addressbook/index.php")
+
+    # def is_element_present(self, how, what):
+    #     try:
+    #         self.wd.find_element(by=how, value=what)
+    #     except NoSuchElementException as e:
+    #         return False
+    #     return True
+    #
+    # def is_alert_present(self):
+    #     try:
+    #         self.wd.switch_to_alert()
+    #     except NoAlertPresentException as e:
+    #         return False
+    #     return True
+    #
+    # def tearDown(self):
+    #     self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
