62ea8133003f3e339bbf42b6a4fe2f9d132289c9
daniilsboev <danilo-web@mail.ru>
2021-03-24 20:42:30 -0400

    fixture created, BUT not working on mac - cannt run on pytest

---
+++ application.py
@@ -0,0 +1,43 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class Application:
+
+    def __init__(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_group(self, group):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_xpath("//div[@id='content']/h1").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, username, password):
+        wd = self.wd
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_homepage(self, homepage_url):
+        wd = self.wd
+        wd.get(homepage_url)
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -1,77 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        self.open_homepage("http://localhost/addressbook/index.php")
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="test", header="test", footer="test"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.open_homepage("http://localhost/addressbook/index.php")
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_xpath("//div[@id='content']/h1").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_homepage(self, homepage_url):
-        wd = self.wd
-        wd.get(homepage_url)
-
-    # def is_element_present(self, how, what):
-    #     try:
-    #         self.wd.find_element(by=how, value=what)
-    #     except NoSuchElementException as e:
-    #         return False
-    #     return True
-    #
-    # def is_alert_present(self):
-    #     try:
-    #         self.wd.switch_to_alert()
-    #     except NoAlertPresentException as e:
-    #         return False
-    #     return True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.open_homepage("http://localhost/addressbook/index.php")
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="test", header="test", footer="test"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.open_homepage("http://localhost/addressbook/index.php")
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
